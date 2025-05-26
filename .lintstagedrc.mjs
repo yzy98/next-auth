4dcbb467663b80e10d4d406807784c794378fef2
yzy98 <yz186337@163.com>
2025-05-26 15:30:39 +0800

    eslint fix

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,12 +1,13 @@
-import path from 'path';
- 
-const buildEslintCommand = (filenames) =>
-  `next lint --fix --file ${filenames
-    .map((f) => path.relative(process.cwd(), f))
-    .join(' --file ')}`
- 
+import path from "node:path";
+
+function buildEslintCommand(filenames) {
+  return `next lint --fix --file ${filenames
+    .map(f => path.relative(process.cwd(), f))
+    .join(" --file ")}`;
+}
+
 const config = {
-  '*.{js,jsx,ts,tsx}': [buildEslintCommand],
+  "*.{js,jsx,ts,tsx}": [buildEslintCommand],
 };
 
 export default config;
